528e5897d6c1b365138f3d52cb55acbfbd570c7c
Mayuradlak212 <156865950+Mayuradlak212@users.noreply.github.com>
2024-08-28 19:55:27 +0530

    Add files via upload

---
+++ js/day1.js
@@ -0,0 +1,10 @@
+console.log("  Welcome to the JavaScript  ");
+//This is the first line of the javascript file
+// document.write("Hii I am Learning JavaScript")
+// alert("I am learning JavaScript")
+// confirm("Do you want to learn JavaScript")
+// prompt("Hii enter your favorite programming language");
+/*
+Hello 
+This is 
+JS Code*/
---
+++ js/day2.js
@@ -0,0 +1,31 @@
+console.log("Hello Welcome Learning of Day2 of JS ")
+
+
+let age =20;
+console.log("Age ",age)
+
+/*
+Variable Decleration 
+
+*/
+/*
+String 
+Number Or Integer 
+BigInt
+Booolean
+Undefined
+Null
+Object
+*/
+
+
+//Operators
+/*
+
+Assigment operator   
+Comparison operator
+Bitwise operator
+Turnery operator
+Logical operator
+Arithmetic operator
+*/
