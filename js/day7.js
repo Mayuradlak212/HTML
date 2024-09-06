b30d334c152e23b8f118ea8775fcc0b334810575
Mayuradlak212 <156865950+Mayuradlak212@users.noreply.github.com>
2024-09-06 19:56:13 +0530

    Add files via upload

---
+++ js/day7.js
@@ -0,0 +1,16 @@
+console.log("Browser Function  in JavaScript")
+
+/*
+console-> Console is Use to Debug the Code of JavaScript and use to view logs 
+  Welcome --> Welcome Tab Contains The Basic information about browser and there features 
+  Element Tab--> Is is use to Represent the structure of the    Current page DOM source code 
+  Source --> It is Use to See the Source Code of the Web and Linked Script file 
+  Network : Network is Used to see the Information of Third Party or CDN Request and Response Details
+  Performance : Performance Tab is Used to View the Performance of the Website
+  Memory: Memory Tab is Used to View the Memory Consumtion of the Website and Storage 
+  LocalStorage : Localstorage is Used to Store data in Browser Permanetly 
+  Session Storage : Is used to Store data in Browser Temperery 
+  */
+
+
+  
---
+++ js/day8.js
@@ -0,0 +1,50 @@
+console.log("Session Storage In JavaScript");
+
+
+
+// Storage Data 
+// Get Data
+// Delete Data 
+
+console.log("Session Storage  Data --> Store")
+
+// it wil staore data in key value pair 
+// , setItem(key, value) will 
+
+// sessionStorage.setItem("name","Mayur Adlak")//Use to Store Data 
+// sessionStorage.setItem("college","OIST Indore M.P ");
+// sessionStorage.setItem("username","mayur_adlak");
+
+// How you can get The Data from the session
+// getItem(key) it will return the value of the key
+let userName =sessionStorage.getItem("name");
+
+console.log("Session Storage Data --> Get ",userName);
+
+// Deleting Data using key 
+// removeItem(key) it will delete  the value of the key
+// sessionStorage.removeItem("college");
+
+// Clear session storage data
+// clear();
+// sessionStorage.clear();
+
+
+
+
+
+console.log("Local Storage ")
+
+// Create local storage
+localStorage.setItem("name","Mayur Adlak");
+localStorage.setItem("college","OIST Indore ");
+
+// Get local storage data
+
+// localStorage.removeItem
+console.log("Local Storage Data --> Get ");
+
+
+localStorage.removeItem("name");
+// localStorage.clear()
+// Delete local storage data
