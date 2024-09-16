41aa3cb1149599328d1b30bf711edacaa822e53a
Mayuradlak212 <156865950+Mayuradlak212@users.noreply.github.com>
2024-09-16 20:00:13 +0530

    Add files via upload

---
+++ js/day14.js
@@ -0,0 +1,21 @@
+
+// setTimeout
+// setInterval
+// clearInterval
+// Spread Opretor ,Destructuring
+
+console.log("Set Time Interval ");
+
+
+
+const printMyName=()=>{
+    console.log("My Name is Mayur ");
+    console.log(" I am Lead Software Engineer")
+}
+const oneTime = ()=>{
+    console.log("One Time Message")
+}
+
+setTimeout(oneTime,2000)
+
+setInterval(printMyName,2000)
